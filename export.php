145ea74304724d3c0e599a4586aec03216823854
Hitendra Rathore <hitendra1995@gmail.com>
2020-06-08 06:25:00 +0530

    added string filter function and download headers

--- export.php
+++ export.php
@@ -1,23 +1,30 @@
 <?php
 //Exporting data to excel sheet
 session_start();
+
 $fileName = "domain_data" . rand(1,100) . ".xls";
 
-if ($_SESSION["domain_data"]) {
-    function filterData($str) {
+if ($_SESSION["domain_data"]) 
+{
+    function filterData($str) 
+    {
         $str = preg_replace("/\t/", "\\t", $str);
         $str = preg_replace("/\r?\n/", "\\n", $str);
         if(strstr($str, '"')) $str = '"' . str_replace('"', '""', $str) . '"';
     }
 
-    // headers for download
-   header("Content-Disposition: attachment; filename=\"$fileName\"");
-   header("Content-Type: application/vnd.ms-excel");
+
+    header("Content-Disposition: attachment; filename=\"$fileName\"");
+    header("Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+    header("Cache-Control: max-age=0");
 
 
     $flag = false;
-    foreach($_SESSION["domain_data"] as $row) {
-        if(!$flag) {
+
+    foreach($_SESSION["domain_data"] as $row) 
+    {
+        if(!$flag) 
+        {
             // display column names as first row
             echo implode("\t", array_keys($row)) . "\n";
             $flag = true;
